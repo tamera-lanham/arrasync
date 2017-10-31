221dc3452573e13ea41be5b41463bdcff66e9b0d
Morgis, Matt (ELS-PHL) <m.morgis@elsevier.com>
2017-10-31 14:23:37 -0400

    chore: setup prettier, wallaby, and maybe eslint

--- src/sum.test.js
+++ src/sum.test.js
@@ -2,4 +2,4 @@ const sum = require('./sum');
 
 test('blah', async () => {
   expect(sum(1, 2)).toBe(3);
-})
+});
---
+++ wallaby.conf.js
@@ -0,0 +1,14 @@
+module.exports = function() {
+  return {
+    files: ['src/**/*.js', '!src/**/*.test.js'],
+
+    tests: ['src/**/*.test.js'],
+
+    env: {
+      type: 'node',
+      runner: 'node',
+    },
+
+    testFramework: 'jest',
+  };
+};
