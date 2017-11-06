ad64f6e57e50d23860634c3a0c445901b10e8d9a
Tamera Lanham <t.lanham@elsevier.com>
2017-11-06 14:47:17 -0500

    refactor: make zip cleaner by using map

--- src/map.js
+++ src/map.js
@@ -1,3 +1,22 @@
+/**
+ * @description Mapper function
+ * 
+ * @callback mapperFunction
+ * @param value - Value of the array
+ * @param {number} i - index of the array
+ * @param array - the array being mapped
+ */
+
+/**
+  * @description Apply a function to every element of an array, returning an array of the 
+  * results.
+  *
+  * @function map
+  * @param {Array} array - The array to be mapped
+  * @param {mapperFunction} func - The function to be called for 
+  * each value of array. Optionally can take the index of each value and the 
+  * array being mapped as arguments.
+  */
 const map = async (array, func) => {
   const promises = [];
   for (const [i, value] of array.entries()) {
