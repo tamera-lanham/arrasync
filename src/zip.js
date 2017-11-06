a1231195bcda3620e57af6db8fdd46e47023ba47
Tamera Lanham <t.lanham@elsevier.com>
2017-11-06 14:59:25 -0500

    refactor: make zip cleaner by using map, this time with the right file

--- src/zip.js
+++ src/zip.js
@@ -1,22 +1,15 @@
+const map = require('./map');
+
 const zip = async (...arrays) => {
-  // TODO: make this more async with reduce
   for (const array of arrays) {
     if (array.length !== arrays[0].length) {
       throw new Error('Cannot zip arrays of unequal length');
     }
   }
 
-  const results = [];
-  for (let i = 0; i < arrays[0].length; i++) {
-    const subList = [];
-
-    for (const array of arrays) {
-      subList.push(array[i]);
-    }
-    results.push(subList);
-  }
-
-  return results;
+  return map(arrays[0], async (value, i) => {
+    return map(arrays, arr => arr[i]);
+  });
 };
 
 const unzip = async array => {
