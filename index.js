b73c3cba34497b18060e9e9d4192e383beb595b3
Tamera Lanham <t.lanham@elsevier.com>
2017-11-06 10:49:42 -0500

    chore: export functions from index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import {filter} from './src/filter';
+import {flatten} from './src/flatten';
+import {map} from './src/map';
+import {reduce} from './src/reduce';
+import {unzip, zip} from './src/zip';
+module.exports = {filter, flatten, map, reduce, unzip, zip};
