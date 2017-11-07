fbfc23c64e86016741192f4c075b98fe4704a939
Tamera Lanham <t.lanham@elsevier.com>
2017-11-07 08:49:33 -0500

    fix: switch imports to require

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import {filter} from './src/filter';
-import {flatten} from './src/flatten';
-import {map} from './src/map';
-import {reduce} from './src/reduce';
-import {unzip, zip} from './src/zip';
+const {filter} = require('./src/filter');
+const {flatten} = require('./src/flatten');
+const {map} = require('./src/map');
+const {reduce} = require('./src/reduce');
+const {unzip, zip} = require('./src/zip');
+
 module.exports = {filter, flatten, map, reduce, unzip, zip};
