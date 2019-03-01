19f6a7e4315afa05c4c0384f5aa64ed58c3134b1
Matt Morgis (ELS-PHI) <m.morgis@elsevier.com>
2019-03-01 10:09:51 -0500

    docs: update func to use number and Array types

--- src/map.js
+++ src/map.js
@@ -4,9 +4,9 @@
   *		
   * @function map		
   * @param {Array} array - The array to be mapped		
-  * @param {function(value, i, array)} func - The function to be called for 		
-  * each value of array. Optionally can take the index of each value and the 		
-  * array being mapped as arguments.
+  * @param {function(value, number, Array): Promise<any>} func - The async 
+  * function to be called for each value of array. Optionally can take the 
+  * index of each value and the array being mapped as arguments.
 */
 const map = async (array, func) => {
   const promises = [];
